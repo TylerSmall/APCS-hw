b066ee0af738f11efddcd4cbea8f68da15b4d93f
TylerSmall <tas10yrs@gmail.com>
2014-10-03 05:54:38 -0400

    Homework 7a, b

--- 07-shapes/Shapes.java
+++ 07-shapes/Shapes.java
@@ -49,4 +49,56 @@ public class Shapes {
         }
         return out;
     }
-}
+
+	public String tri3(int h){
+		String s = "";
+		for(int i=0; i<h; i++){
+		    for(int j=0; j<=h+i; j++){
+			if(j+i<h-1){
+			    s=s+" ";
+			}else{
+			    if(j==h+i){
+				s=s+"\n";
+			    }else{
+				s=s+"*";
+			    }
+			}
+		    }
+		}
+		return s;	
+	    }
+	
+	    public String diamond(int h){
+		if(h%2==0){
+		    return "Input an odd number \n";
+		}
+		int mid=(h-1)/2;
+		String s="";
+		for(int i=0; i<h; i++){
+		    for(int j=0; j<h; j++){
+			if(Math.abs(mid-i)+Math.abs(mid-j)>mid){
+			    s=s+" ";
+			}else{
+			    s=s+"*";
+			}
+		    }
+		    s=s+"\n";
+		}
+		return s;
+	    }
+	
+	    public String tri4(int h){
+		String s = "";
+		for(int i=0; i<h; i++){
+		    for(int j=0; j<h; j++){
+			if(i-j>0){
+			    s=s+" ";
+			}else{
+			    s=s+"*";
+			}
+		    }
+		    s=s+"\n";
+		}
+		return s;
+	    }
+	}
