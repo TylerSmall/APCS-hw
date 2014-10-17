ef9f34b93bdaa269ba5b631d53be2e56c0e3781e
Tyler Small <tas10yrs@gmail.com>
2014-10-17 09:27:32 -0400

    Stuyablo

--- Stuyablo/BaseChar.java
+++ Stuyablo/BaseChar.java
@@ -6,10 +6,17 @@ public class BaseChar {
     private int Mana = 10;
     private int Attack = 20;
     private int Dexterity = 20;
-    public void toName(){
-	Scanner sc = new Scanner(System.in);
-	System.out.println("What is your name?");
-	String name = sc.nextLine();
-	System.out.println("Your name is " + name);
+    public void toCreate(){
+        String n = "";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your name?");
+        n += sc.nextLine();
+	String c = "";
+	Scanner sc2 = new Scanner (System.in);
+	while !(line.equals("warrior") || line.equals("mage")){
+		System.out.println("Are you a Warrior or a Mage?");
+	String line = sc2.nxtLine().toLowerCase;
+	c += sc.nextLine();
+	System.out.println("You are " + n + " the " + c);
     }
 }
