27561a7bc3e9ab8bfb5931568e6979d1c29bbde8
Tyler Small <tas10yrs@gmail.com>
2014-10-28 09:27:31 -0400

    Homework 14

--- 14-Arraystuff/Arraystuff.java
+++ 14-Arraystuff/Arraystuff.java
@@ -18,5 +18,21 @@ public class Arraystuff {
 	}
 	return sum;
     }
+
+    public boolean more14(int[] nums) {
+	int count1 = 0;
+	int count4 = 0;
+	for (int i=0; i<nums.length; i++) {
+	    if (nums[i] = 1) {
+		count1 += 1;
+	    } else if (nums[i] = 4) {
+		count4 += 1;
+	    }
+	}
+	if (count1 > count4) {
+	    return true;
+	}
+    }
+	    
 		       
 	        
